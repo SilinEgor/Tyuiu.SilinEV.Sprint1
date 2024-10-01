17b1229fd3f85f5111f2c08f0bde6bab484b56dd
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-10-01 21:07:38 +0500

    Add project files.

---
+++ Tyuiu.SilinEV.Sprint1.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint1.Task0.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint1.Task0.V2/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.SilinEV.Sprint1.Task0.V2.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Силин Е. В. | РППб-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #0                                                              *");
+        Console.WriteLine("* Выполнил: Силин Егор Вадимович | РППб-24-1                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет выражение 3/3*2+12/4/3-2          *");
+        Console.WriteLine("* и печатает результат на экране.                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* 3/3*2+12/4/3-2                                                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate());
+
+        Console.ReadKey();
+    }
+}
