182b72de88a4271514aca4ec448d757a07e1cf43
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-10-02 01:43:43 +0500

    Task2

---
+++ Tyuiu.SilinEV.Sprint1.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint1.Task2.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint1.Task2.V6/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.SilinEV.Sprint1.Task2.V6.Lib;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int x;
+
+Console.WriteLine("Введите значение X:");
+x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Квадрат числа X = " + ds.ConvertMToKm(x));
+
+Console.ReadLine();
