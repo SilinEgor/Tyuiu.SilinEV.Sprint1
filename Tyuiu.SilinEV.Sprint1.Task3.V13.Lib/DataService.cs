0460de3d46661cdb25c6ea0a92b8275ae34f1102
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-10-02 02:43:12 +0500

    Task3

--- Tyuiu.SilinEV.Sprint1.Task3.V13.Lib/DataService.cs
+++ Tyuiu.SilinEV.Sprint1.Task3.V13.Lib/DataService.cs
@@ -4,10 +4,15 @@ namespace Tyuiu.SilinEV.Sprint1.Task3.V13.Lib
 {
     public class DataService : ISprint1Task3V13
     {
-        public double MultiplyOfDigits(double number)
+        public double MultiplyOfDigits(double a)
         {
-            string a = Convert.ToString(number);
-            return a[0] * a[1] * a[2];
+            int[] s = new int[3];
+            string d = Convert.ToString(a);
+            for (int i = 0; i < s.Length; i++)
+            {
+                s[i] = Convert.ToInt32(d[i]);
+            }
+            return s[0] * s[1] * s[2];
         }
     }
 }
