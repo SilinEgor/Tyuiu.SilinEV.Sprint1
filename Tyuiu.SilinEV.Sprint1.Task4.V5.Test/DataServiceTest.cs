0c06f0456edad714af7e7faaa228c273afc4e0f5
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-10-04 20:04:31 +0500

    Task4

---
+++ Tyuiu.SilinEV.Sprint1.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint1.Task4.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint1.Task4.V5/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.SilinEV.Sprint1.Task4.V5.Lib;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int x, y;
+
+Console.WriteLine("Введите значение X:");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение Y:");
+y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("1 / |x + sqrt(y)| = " + ds.Calculate(x, y));
+
+Console.ReadKey();
